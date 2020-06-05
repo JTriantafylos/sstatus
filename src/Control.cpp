4674a58f051cb24645082767e4112331b44838a9
James Triantafylos <JTriantafylos@gmail.com>
2020-06-05 16:17:01 -0400

    Fixed indentation

--- src/Control.cpp
+++ src/Control.cpp
@@ -3,39 +3,39 @@
 using namespace std;
 
 void Control::launch() {
-	mStreamWriter.initJSONStream();
-	initItems();
+    mStreamWriter.initJSONStream();
+    initItems();
 
-	while(true) {
-		generateStatus();
-		usleep(REFRESH_RATE);
-	}
+    while(true) {
+        generateStatus();
+        usleep(REFRESH_RATE);
+    }
 }
 
 void Control::generateStatus() {
-	mStreamWriter.beginStatusItemArray();
-	for(std::vector<StatusItem*>::iterator it = mItems.begin(); it != mItems.end(); ++it) {
-		bool lastItem = false;
-		if(it + 1 == mItems.end())
-			lastItem = true;
-		StatusItem* item = *it;
-		mStreamWriter.writeStatusItem(item->getJsonText(), lastItem);
-	}
-	mStreamWriter.endStatusItemArray();
+    mStreamWriter.beginStatusItemArray();
+    for(std::vector<StatusItem*>::iterator it = mItems.begin(); it != mItems.end(); ++it) {
+        bool lastItem = false;
+        if(it + 1 == mItems.end())
+            lastItem = true;
+        StatusItem* item = *it;
+        mStreamWriter.writeStatusItem(item->getJsonText(), lastItem);
+    }
+    mStreamWriter.endStatusItemArray();
 }
 
 void Control::initItems() {
-	StatusItem* mItem0 = new StatusItemSeparator();
-	StatusItem* mItem1 = new StatusItemPadding(4);
-	StatusItem* mItem2 = new StatusItemSeparator();
-	StatusItem* mItem3 = new StatusItemPadding(2);
-	StatusItem* mItem4 = new StatusItemSeparator();
-	StatusItem* mItem5 = new StatusItemPadding(8);
+    StatusItem* mItem0 = new StatusItemSeparator();
+    StatusItem* mItem1 = new StatusItemPadding(4);
+    StatusItem* mItem2 = new StatusItemSeparator();
+    StatusItem* mItem3 = new StatusItemPadding(2);
+    StatusItem* mItem4 = new StatusItemSeparator();
+    StatusItem* mItem5 = new StatusItemPadding(8);
 
-	mItems.push_back(mItem0);
-	mItems.push_back(mItem1);
-	mItems.push_back(mItem2);
-	mItems.push_back(mItem3);
-	mItems.push_back(mItem4);
-	mItems.push_back(mItem5);
+    mItems.push_back(mItem0);
+    mItems.push_back(mItem1);
+    mItems.push_back(mItem2);
+    mItems.push_back(mItem3);
+    mItems.push_back(mItem4);
+    mItems.push_back(mItem5);
 }
