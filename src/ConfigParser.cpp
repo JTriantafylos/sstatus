85cb4e5fc81392c263ec5994c9763af4598eab52
James Triantafylos <JTriantafylos@gmail.com>
2021-05-06 00:21:19 -0400

    Refactor ConfigParser error handling to be more descriptive
    Refactored the ConfigParser error handling code to be more descriptive
    in the error messages it gives. Also moved to using exceptions for all
    error handling as opposed to jerry-rigging StatusItem objects to act as
    error messages.

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -10,48 +10,51 @@ std::vector<std::shared_ptr<StatusItem>> ConfigParser::loadStatusItemsFromConfig
         throw err;
     }
 
-    if (config["Order"].is_array()) {
-        toml::array* orderArray = config["Order"].as_array();
-        for (int i = 0; i < orderArray->size(); ++i) {
-            const toml::node* orderItem = orderArray->get(i);
-            if (!orderItem->is_string()) {
-                std::string errorMessage("Invalid status item listed in order array at position [" + std::to_string(i) +
-                                         "]!");
-                throw std::runtime_error(errorMessage);
-            }
-
-            std::string statusItemName = orderItem->as_string()->get();
-            if (!config[statusItemName]) {
-                std::string errorMessage("Non-existent status item '" + statusItemName + "' listed in order array!");
-                throw std::runtime_error(errorMessage);
-            }
-
-            // TODO: Implement other status item fields
-            std::shared_ptr<StatusItem> item;
-            toml::table* table = config[statusItemName].as_table();
-            if (isValidStatusItem(*table)) {
-                std::string in = std::string(statusItemName + "_" + std::to_string(i));
-                std::string s = table->get("Script")->value_or("");
-                std::string fg = table->get("ForegroundColor")->value_or("");
-                std::string bg = table->get("BackgroundColor")->value_or("");
-                std::string bc = table->get("BorderColor")->value_or("");
-                bool sep = table->get("SeparatorAfter")->value_or(true);
-                long interval = table->get("Interval")->value_or(-1);
-                item = std::make_shared<StatusItem>(statusItemName, in, s, fg, bg, bc, sep, interval);
-            } else {
-                item = std::make_shared<StatusItem>("", "", "echo \"Malformed status item configuration!\"", "#ff0000",
-                                                    "", "", true, -1);
-            }
-            items.push_back(item);
-        }
-    } else {
+    if (!config["Order"].is_array()) {
         throw std::runtime_error("Status item order array is not defined!");
+    }
+
+    toml::array* orderArray = config["Order"].as_array();
+    for (int i = 0; i < orderArray->size(); ++i) {
+        const toml::node* orderItem = orderArray->get(i);
+        if (!orderItem->is_string()) {
+            std::string errorMessage("Invalid status item in order array at position [" + std::to_string(i) + "]!");
+            throw std::runtime_error(errorMessage);
+        }
+
+        std::string statusItemName = orderItem->as_string()->get();
+        if (!config[statusItemName]) {
+            std::string errorMessage("Non-existent status item '" + statusItemName + "' in order array!");
+            throw std::runtime_error(errorMessage);
+        }
+
+        toml::table* table = config[statusItemName].as_table();
+        if (!isValidStatusItem(*table)) {
+            std::string errorMessage("Malformed configuration for status item: '" + statusItemName + "'!");
+            throw std::runtime_error(errorMessage);
+        }
+
+        // TODO: Implement other status item fields
+        std::shared_ptr<StatusItem> item;
+
+        std::string in = std::string(statusItemName + "_" + std::to_string(i));
+        std::string s = table->get("Script")->value_or("");
+        std::string fg = table->get("ForegroundColor")->value_or("");
+        std::string bg = table->get("BackgroundColor")->value_or("");
+        std::string bc = table->get("BorderColor")->value_or("");
+        bool sep = table->get("SeparatorAfter")->value_or(true);
+        long interval = table->get("Interval")->value_or(-1);
+
+        item = std::make_shared<StatusItem>(statusItemName, in, s, fg, bg, bc, sep, interval);
+        items.push_back(item);
     }
     return items;
 }
+
+// TODO: If invalid, specify which field(s) is invalid
 bool ConfigParser::isValidStatusItem(const toml::table& table) {
-    if (table["Script"] && table["ForegroundColor"] && table["BackgroundColor"] && table["BorderColor"] &&
-        table["Interval"]) {
+    if (table["Script"].is_string() && table["ForegroundColor"].is_string() && table["BackgroundColor"].is_string() &&
+        table["BorderColor"].is_string() && table["SeparatorAfter"].is_boolean() && table["Interval"].is_number()) {
         return true;
     } else {
         return false;
