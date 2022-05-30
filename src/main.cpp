5b6f76c66448ce2ff7b282b5be2f696d7ad9cc26
James Triantafylos <jtriantafylos@protonmail.com>
2022-05-30 17:37:47 -0400

    Add options parsing TODO comment

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 int main(int argc, char* argv[]) {
     Control control;
 
+    // TODO: Implement proper options parsing
     if (argc == 2) {
         control.launch(std::string(argv[1]));
     } else {
